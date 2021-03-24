e1d37bc15b5b58cfe7ea112b8f9a827ea73fc64a
Luis Palacios <lrpalaciosdev@gmail.com>
2021-03-24 11:30:19 -0600

    add initial implementation of web3modal

--- demo/nextjs-demo/pages/index.tsx
+++ demo/nextjs-demo/pages/index.tsx
@@ -2,12 +2,41 @@ import Head from "next/head";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "../styles/Home.module.css";
-import { UniversalWallet } from '../../../lib'
+import { DIDManager } from '../../../lib';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export default function Home() {
   const onConnectClick = async () => {
-    const universalWallet = new UniversalWallet();
-    const demo = await universalWallet.unlock('1234', 'abcdef123456');
+    const CERAMIC_URL = 'dev-ceramic.paidnetwork.com'; // TODO: set as env variable
+    const providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          infuraId: 'bce97999b34a4b759ca27229313f96ec' // TODO: set as env variable
+        }
+      },
+    }
+    const web3Modal = new Web3Modal({
+      providerOptions: providerOptions,
+      cacheProvider: false
+    })
+    const provider = await web3Modal.connect();
+    const addresses = await provider.enable()
+    debugger;
+
+    const didManager = new DIDManager();
+    // let address = '';
+    // if (provider.accounts)
+    // // walletconnect
+    //   address = provider.accounts[0];
+    // else{
+    //   // metamask
+    //   address =  provider.selectedAddress;
+    // }
+    const externalWeb3 =  await didManager.create3IDWeb3External(provider, addresses[0]); // TODO: send ceramic as third argument, 
+    await externalWeb3.did.authenticate();
+
     toast.success("Connected");
   };
 
