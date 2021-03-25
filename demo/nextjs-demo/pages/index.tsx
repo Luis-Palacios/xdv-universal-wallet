316ef8e811b4d9c77a84e94ec51e88d7ee0c7d13
Luis Palacios <lrpalaciosdev@gmail.com>
2021-03-24 23:02:15 -0600

    add ceramic initial implementation

--- demo/nextjs-demo/pages/index.tsx
+++ demo/nextjs-demo/pages/index.tsx
@@ -1,41 +1,72 @@
 import Head from "next/head";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { DIDManager } from '../../../lib';
-import Web3Modal from 'web3modal';
+// import { DIDManager } from '../../../lib';
+
+import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+
+import ThreeIdResolver from "@ceramicnetwork/3id-did-resolver";
+import Ceramic from "@ceramicnetwork/http-client";
+import { DID } from "dids";
+import { IDX } from "@ceramicstudio/idx";
+import { ThreeIdConnect, EthereumAuthProvider } from "3id-connect";
+
+const CERAMIC_URL = "http://localhost:7007"; // TODO: set as env variable
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "bce97999b34a4b759ca27229313f96ec", // TODO: set as env variable
+    },
+  },
+};
 
 export default function Home() {
   const onConnectClick = async () => {
-    const CERAMIC_URL = 'dev-ceramic.paidnetwork.com'; // TODO: set as env variable
-    const providerOptions = {
-      walletconnect: {
-        package: WalletConnectProvider,
-        options: {
-          infuraId: 'bce97999b34a4b759ca27229313f96ec' // TODO: set as env variable
-        }
-      },
-    }
-    const web3Modal = new Web3Modal({
+    const ethProvider = await new Web3Modal({
       providerOptions: providerOptions,
-      cacheProvider: false
-    })
-    const provider = await web3Modal.connect();
-    const addresses = await provider.enable()
-    debugger;
+      cacheProvider: false,
+    }).connect();
 
-    const didManager = new DIDManager();
-    // let address = '';
-    // if (provider.accounts)
-    // // walletconnect
-    //   address = provider.accounts[0];
-    // else{
-    //   // metamask
-    //   address =  provider.selectedAddress;
-    // }
-    const externalWeb3 =  await didManager.create3IDWeb3External(provider, addresses[0]); // TODO: send ceramic as third argument, 
-    await externalWeb3.did.authenticate();
+    const addresses = await ethProvider.enable();
+
+    debugger;
+    const authProvider = new EthereumAuthProvider(ethProvider, addresses[0]);
+    const threeIdConnect = new ThreeIdConnect();
+    await threeIdConnect.connect(authProvider);
+    const didProvider =  threeIdConnect.getDidProvider();
+
+    debugger;
+    const ceramic = new Ceramic(CERAMIC_URL);
+    ceramic.setDIDProvider(didProvider);
+    
+    // ceramic.setDIDProvider
+    // const did = new DID({
+    //   provider: threeIdConnect.getDidProvider(),
+    //   resolver: ThreeIdResolver.getResolver(ceramic),
+    // });
+
+    await ceramic.did.authenticate();
+
+    console.log(ceramic.did.id);
+
+    const jws = await ceramic.did.createJWS({ hello: "world" });
+    console.log(jws);
+    const aliases = {
+      secretNotes: 'kjzl6cwe1jw14b03qkg5rl0dmq44yjayvku5yvca69fhokexzodwpbjb2zgqusj'
+    }
+    window.idx = new IDX({ ceramic, aliases });
+    window.ceramic = ceramic;
+    window.did = ceramic.did.id;
+
+    // const externalWeb3 = await didManager.create3IDWeb3External(
+    //   provider,
+    //   addresses[0]
+    // ); // TODO: send ceramic as third argument,
+    // await externalWeb3.did.authenticate();
 
     toast.success("Connected");
   };
