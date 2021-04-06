b868238a0a17f232e4567f840088a84ec1c499f2
Luis Palacios <lrpalaciosdev@gmail.com>
2021-04-06 17:55:19 -0600

    add working demo 'final'

--- demo/nextjs-demo/pages/index.tsx
+++ demo/nextjs-demo/pages/index.tsx
@@ -1,36 +1,34 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Wallet,  } from '../../../lib'
+import { Wallet } from '../../../lib'
 
 export default function Home() {
   const onConnect = async () => {
     // @ts-ignore
+
+    const wallet = new Wallet({ isWeb: true});
+
     const addresses = await window.ethereum.enable();
     const address = addresses[0];
+    
     // Password 12 characters or more
     const passphrase = 'qwerty123456';
     const accountName = 'molekilla';
-
     
-    const wallet = new Wallet();
+    // Create DB
     await wallet.open(accountName, passphrase)
 
-    // Enroll account only needs to done once
-    // Returns account if already created
+    // Add collection ?
     await wallet.enrollAccount({
       passphrase,
       accountName,
     });
-
-    let acct = await wallet.getAccount()
-    const walletId = await wallet.addWallet();
     
     let did = await wallet.getDIDAccountFromWeb3Address(address);
     if(!did) {
       did = await wallet.create3ID(address);
     } 
 
-    console.log(did);
   }
 
   return (
